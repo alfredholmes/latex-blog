635b5da495b94655bb78f575a786da37eb2818d4
Alfred <holmesey55@gmail.com>
2022-07-24 13:19:07 +0100

    Http404 and Ordering recent posts

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Post, AboutSection, Category, TitleElement, SocialLink
 
@@ -11,7 +12,7 @@ def get_title_data():
 
 def index(request):
     data = get_title_data()
-    data['post_list'] = Post.objects.all()
+    data['post_list'] = Post.objects.all().order_by('publication_date')
     return render(request, 'posts/index.html', data)
 
 def post(request, post_slug):
@@ -21,13 +22,14 @@ def post(request, post_slug):
         content = post.renderedpage_set.all()[0] 
         header = content.head 
         body = content.body
+    
+        data['post'] = post
+        data['header'] = header
+        data['body'] = body
+        return render(request, 'posts/post.html', data)
     except Post.DoesNotExist:
         raise Http404("Post does not exist")
 
-    data['post'] = post
-    data['header'] = header
-    data['body'] = body
-    return render(request, 'posts/post.html', data)
 
 
 def post_page(request, post_slug, html_slug):
@@ -37,27 +39,30 @@ def post_page(request, post_slug, html_slug):
         content = post.renderedpage_set.filter(name='.'.join(html_slug.split('.')[:-1]))[0] 
         header = content.head 
         body = content.body
-    except Post.DoesNotExist:
+        data['post'] = post
+        data['header'] = header
+        data['body'] = body
+        return render(request, 'posts/post.html', data)
+    except IndexError:
+        raise Http404("Page does not exist")
+    except Page.DoesNotExist:
         raise Http404("Post does not exist")
 
-    data['post'] = post
-    data['header'] = header
-    data['body'] = body
-
-    return render(request, 'posts/post.html', data)
 
 def collections(request):
     #get collections and <=5 most recent posts from each, order by recently updated
     data = get_title_data() 
-    collections = Category.objects.all()
-    posts = {}
-    for collection in collections: 
-        posts[collection] = collection.post_set.all().reverse()[:5]
+    try:
+        collections = Category.objects.all()
+        posts = {}
+        for collection in collections: 
+            posts[collection] = collection.post_set.all().reverse()[:5]
 
-    data['collections'] = posts
+        data['collections'] = posts
 
-    return render(request, 'posts/collections.html', data)
-
+        return render(request, 'posts/collections.html', data)
+    except:
+        raise Http404("Collection Missing")
 
 def collection(request, collection_slug):
     data = get_title_data()
@@ -73,8 +78,12 @@ def collection(request, collection_slug):
     return render(request, 'posts/collections.html', data)
 
 def about(request):
+
     data = get_title_data()
-    sections = AboutSection.objects.all()
-    data['sections'] = sections
-    return render(request, 'posts/about.html', data)
+    try:
+        sections = AboutSection.objects.all()
+        data['sections'] = sections
+        return render(request, 'posts/about.html', data)
+    except:
+        raise Http404('About page does not exist')
     
